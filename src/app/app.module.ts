9d91514d181212a22d16d9fb43d24eed0c904f19
Julio Barros <juliocezarbarros2@gmail.com>
2022-05-31 22:57:47 -0300

    concertando erros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RodapeComponent } from './rodape/rodape.component';
+
 
 
 @NgModule({
