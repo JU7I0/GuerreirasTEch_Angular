6c42fd96f7f5cc74455c1ac6ec86e97ca9572ac8
Julio Barros <83424958+JU7I0@users.noreply.github.com>
2022-05-31 23:11:37 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContatoComponent } from './contato/contato.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContatoComponent
+    ContatoComponent,
+    InicioComponent,
+    SobreComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
