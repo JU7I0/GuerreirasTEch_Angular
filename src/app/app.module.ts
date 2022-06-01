1d4abcfc6defa59b32fe2b9a1d00825bff579262
Julio Barros <juliocezarbarros2@gmail.com>
2022-05-31 23:06:10 -0300

    Adicionando declarations no NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContatoComponent } from './contato/contato.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent,
+    InicioComponent,
+    SobreComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
