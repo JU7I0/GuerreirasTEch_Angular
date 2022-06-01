363f31445afd4c1fdb968c4b83ebd3abe0717cf8
Lavínia Amorim <lavinia.santos2105@gmail.com>
2022-05-31 23:09:23 -0300

    adicionando component menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { ContatoComponent } from './contato/contato.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContatoComponent
+    ContatoComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
