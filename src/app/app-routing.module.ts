bd72443e51549ce1a8ebe66a2e4670641a38484f
Julio Barros <juliocezarbarros2@gmail.com>
2022-06-01 21:23:55 -0300

    Adicionando path no routing module e inserindo caminhos ao app.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContatoComponent } from './contato/contato.component';
+import { InicioComponent } from './inicio/inicio.component';
 import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
   {path : '', redirectTo : 'inicio',pathMatch:'full'},
+  {path: 'inicio', component: InicioComponent},
   {path: 'contato', component : ContatoComponent},
   {path: 'sobre', component: SobreComponent}
 ];
