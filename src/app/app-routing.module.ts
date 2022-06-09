a2c563384e5b4918036d76849d6b112cd6c74c3f
Julio Barros <juliocezarbarros2@gmail.com>
2022-06-09 13:22:50 -0300

    Adicionando rotas para app-tema

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { CategoriaComponent } from './categoria/categoria.component';
 import { ContatoComponent } from './contato/contato.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { SobreComponent } from './sobre/sobre.component';
+
 
 const routes: Routes = [
   {path : '', redirectTo : 'entrar',pathMatch:'full'},
@@ -12,7 +14,8 @@ const routes: Routes = [
   {path: 'contato', component : ContatoComponent},
   {path: 'sobre', component: SobreComponent},
   {path: 'entrar', component: EntrarComponent},
-  {path: 'cadastrar', component: CadastrarComponent}
+  {path: 'cadastrar', component: CadastrarComponent},
+  {path: 'categoria', component: CategoriaComponent}
 ];
 
 @NgModule({
