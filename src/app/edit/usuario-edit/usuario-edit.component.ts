afa802a3968d5dbbb761fc5cd0f08cad7dde330a
Julio Barros <juliocezarbarros2@gmail.com>
2022-06-18 15:43:24 -0300

    Implementação do usuario-edit

---
+++ src/app/edit/usuario-edit/usuario-edit.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Usuario } from 'src/app/model/Usuario';
+import { AuthService } from 'src/app/service/auth.service';
+import { environment } from 'src/environments/environment.prod';
+
+@Component({
+  selector: 'app-usuario-edit',
+  templateUrl: './usuario-edit.component.html',
+  styleUrls: ['./usuario-edit.component.css']
+})
+export class UsuarioEditComponent implements OnInit {
+
+  usuario: Usuario = new Usuario()
+  idusuario: number
+  confirmarSenha: string
+  tipoUsuario: string
+
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0, 0)
+
+    if (environment.token == '') {
+      this.router.navigate(['/entrar'])
+    }
+
+    this.idusuario = this.route.snapshot.params['id']
+    this.findByIdusuario(this.idusuario)
+  }
+
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
+  }
+
+  tipousuario(event: any) {
+    this.tipoUsuario = event.target.value
+  }
+
+  atualizar() {
+    this.usuario.tipo = this.tipoUsuario
+
+    if (this.usuario.senha != this.confirmarSenha) {
+      alert('A senhas estão incorretas.')
+    } else {
+      this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+        this.usuario = resp
+        this.router.navigate(['/inicio'])
+        alert('Usuário atualizado com sucesso, faça o login novamente.')
+        environment.token = ''
+        environment.nome = ''
+        environment.foto = ''
+        environment.id = 0
+
+        this.router.navigate(['/entrar'])
+      })
+    }
+  }
+
+  findByIdusuario(id: number) {
+    this.authService.getByIdUsuario(id).subscribe((resp: Usuario) => {
+      this.usuario = resp
+    })
+  }
+
+}
